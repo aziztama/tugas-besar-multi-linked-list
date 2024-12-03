cff9d9b8891eaa56d25a4cca55c2f790f82cacff
aziztama <azizpratama666@gmail.com>
2024-12-03 21:11:15 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,5 +9,25 @@ using namespace std;
 
 int main() {
 
+    //tes
+    DriverList driverList;
+
+
+
+    driverList.insertDriver(1, "John Doe", "08123456789", "B1234XYZ", "Available");
+    driverList.insertDriver(2, "Jane Smith", "08234567890", "B5678ABC", "Unavailable");
+
+    
+    cout << "Daftar Driver:" << endl;
+    driverList.displayDrivers();
+
+   
+    Driver* driver = driverList.findDriver(1);
+    if (driver) {
+        cout << "Driver ditemukan: " << driver->name << endl;
+    } else {
+        cout << "Driver tidak ditemukan." << endl;
+    }
+
     return 0;
 }
